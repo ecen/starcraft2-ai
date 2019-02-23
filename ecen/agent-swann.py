f0575c9506ada3c17217e0e2a79adcef79b92dd3
ecen <ericgul@student.chalmers.se>
2019-02-23 16:44:06 +0100

    WIP. Added constants and transformLocation function.

--- ecen/agent-swann.py
+++ ecen/agent-swann.py
@@ -8,11 +8,29 @@ from pysc2.agents import base_agent
 from pysc2.lib import actions
 from pysc2.lib import features
 
+FUNCTIONS = actions.FUNCTIONS
+
+# Functions
+_BUILD_SUPPLYDEPOT = actions.FUNCTIONS.Build_SupplyDepot_screen.id
+_NOOP = actions.FUNCTIONS.no_op.id
+_SELECT_POINT = actions.FUNCTIONS.select_point.id
+
+# Features
+_PLAYER_RELATIVE = features.SCREEN_FEATURES.player_relative.index
+_UNIT_TYPE = features.SCREEN_FEATURES.unit_type.index
+
 _PLAYER_SELF = features.PlayerRelative.SELF
 _PLAYER_NEUTRAL = features.PlayerRelative.NEUTRAL  # beacon/minerals
 _PLAYER_ENEMY = features.PlayerRelative.ENEMY
 
-FUNCTIONS = actions.FUNCTIONS
+# Unit IDs
+_TERRAN_COMMANDCENTER = 18
+_TERRAN_SCV = 45
+
+# Parameters
+_PLAYER_SELF = 1
+_NOT_QUEUED = [0]
+_QUEUED = [1]
 
 
 def _xy_locs(mask):
@@ -38,6 +56,20 @@ class SwannMoveToBeacon(base_agent.BaseAgent):
 
 class SwannCraft(base_agent.BaseAgent):
     """Agent for playing the Simple64 map."""
+    base_top_left = None # True if our base is the top left one.
+
+    # Detect if our base is the top left one by taking the mean y-coordinate of our units.
+    if self.base_top_left is None:
+        player_y, player_x = (obs.observation["minimap"][_PLAYER_RELATIVE] == _PLAYER_SELF).nonzero()
+        self.base_top_left = player_y.mean() <= 31
+
+    # Transform coordinates and distances based on where our base is.
+    def transformLocation(self, x, x_distance, y, y_distance):
+        if not self.base_top_left:
+            return [x - x_distance, y - y_distance]
+
+        return [x + x_distance, y + y_distance]
+
     def step(self, obs):
         super(SwannCraft, self).step(obs)
 
