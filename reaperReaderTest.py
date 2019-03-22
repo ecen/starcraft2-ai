6e804f3e834593bc69b9c8362f56a2c6590a17b6
Aoldo <smurfpojken@gmail.com>
2019-03-22 11:53:32 +0100

    Change the format used to store data, can now be read back by using reaperReaderTest. Data is not on the same format as live pysc2 play however so a parser is required before it is used for supervised learning.

---
+++ reaperReaderTest.py
@@ -0,0 +1,33 @@
+import pymongo
+import pickle
+import numpy as np
+from google.protobuf.json_format import ParseDict
+
+
+client = pymongo.MongoClient()
+db = client["replay_database"]
+replays = db["replays"]
+players = db["players"]
+states = db["states"]
+scores = db["scores"]
+
+replays.create_index("replay_id")
+players.create_index("replay_id")
+states.create_index([("replay_id", pymongo.ASCENDING), ("frame_id", pymongo.ASCENDING)])
+scores.create_index([("replay_id", pymongo.ASCENDING), ("frame_id", pymongo.ASCENDING)])
+
+replay_ids = []
+for replay_doc in replays.find({}, {"replay_id": 1}):
+    replay_ids.append(replay_doc["replay_id"])
+
+for replay_id in replay_ids:
+    cursor = states.find({'replay_id':replay_id, 'player_id': 1, 'frame_id': 12})
+    for state_doc in cursor:
+        a = pickle.loads(state_doc["screen"]["factions"])
+        print(a)
+        #print(pickle.loads(state_doc["screen"]["factions"]))
+
+
+#states.find()
+
+#print(replay_ids)
