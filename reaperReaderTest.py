5f0a9b1fdd72beab420970b9e38ff16f5df25ec9
Aoldo <smurfpojken@gmail.com>
2019-03-29 11:39:28 +0100

    Add get function for getting random state from all available in the database.

--- reaperReaderTest.py
+++ reaperReaderTest.py
@@ -1,6 +1,7 @@
 import pymongo
 import pickle
 import numpy as np
+import random
 from google.protobuf.json_format import ParseDict
 import sys
 
@@ -29,6 +30,22 @@ replay_ids = []
 for replay_doc in replays.find({}, {"replay_id": 1}):
     replay_ids.append(replay_doc["replay_id"])
     print(replay_doc["replay_id"])
+
+def queryRandomState():
+    replayID = random.choice(replay_ids)
+    frameID = randomViableFrame(replayID)
+    playerID = random.choice([1,2])
+    return queryState(replayID, frameID, playerID)
+
+def randomViableFrame(replayID):
+    maxFrames = 0
+    for replay in replays.find({"replay_id":replayID}):
+        maxFrames = replay["game_duration_loops"]
+        break;
+    frameID = framesPerStep*(random.randint(0, maxFrames//framesPerStep))
+    return frameID
+
+
 
 def queryState(replayID, frameID, playerID):
     state = states.find({'replay_id':replayID, 'frame_id':frameID, 'player_id':playerID})
@@ -60,7 +77,8 @@ def queryState(replayID, frameID, playerID):
 
         return (concRaw, concMinimap, concScreen)
 
-print(queryState(replay_ids[0],12,1))
+
+#print(queryState(replay_ids[0],12,1))
 #for replay_doc in replays.find():
 #    print(pickle.loads(replay_doc["map"]["minimap"]["height"]))
 
@@ -75,3 +93,8 @@ print(queryState(replay_ids[0],12,1))
 #states.find()
 
 #print(replay_ids)
+def test_randomViableFrame():
+    for i in range(1,1000):
+        if(randomViableFrame(replay_ids[0])%framesPerStep != 0):
+            print("Invalid frameID")
+    print(":)")
