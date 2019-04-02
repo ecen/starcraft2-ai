c2108405e89d5b2f57470d5f86ab07e628e8196b
Aoldo <smurfpojken@gmail.com>
2019-04-02 11:16:00 +0200

    Swap model to be able to use multiple inputs.

--- agent.py
+++ agent.py
@@ -20,25 +20,49 @@ BATCH_SIZE = 20
 FRAME_WIDTH = 64
 FRAME_HEIGHT = 64
 STATE_LENGTH = 9
+NUMERIC_INPUT_LENGTH = 7
 
 class Network:
     def __init__(self):
         # Creation of network, topology stuff goes here
-        self.model = Sequential()
-        self.model.add(
-                Conv2D(64, 4, activation="relu",
-                       input_shape=(FRAME_WIDTH, FRAME_HEIGHT, STATE_LENGTH), data_format='channels_last'))
-        self.model.add(MaxPooling2D(pool_size=(2, 2)))
-        self.model.add(Conv2D(32, 2, activation="relu"))
-        self.model.add(MaxPooling2D(pool_size=(2, 2)))
-        self.model.add(Conv2D(32, 2, activation="relu"))
-        self.model.add(MaxPooling2D(pool_size=(2, 2)))
-        self.model.add(Flatten())
-        self.model.add(Dense(512, activation="relu"))
-        self.model.add(Dense(1, activation="tanh")) #1=win, 0=lose, not sure if it's a direct correlation to % win/loss
+        #self.model = Sequential()
+        #self.model.add(
+        #        Conv2D(64, 4, activation="relu",
+        #               input_shape=(FRAME_WIDTH, FRAME_HEIGHT, STATE_LENGTH), data_format='channels_last'))
+        #self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        #self.model.add(Conv2D(32, 2, activation="relu"))
+        #self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        #self.model.add(Conv2D(32, 2, activation="relu"))
+        #self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        #self.model.add(Flatten())
+        #self.model.add(Dense(512, activation="relu"))
+        #self.model.add(Dense(1, activation="tanh")) #1=win, 0=lose, not sure if it's a direct correlation to % win/loss
+        #self.model.compile(loss="mean_squared_error", optimizer=Adam(lr=LEARNING_RATE))
 
+        convInput = keras.layers.Input(shape=(FRAME_WIDTH, FRAME_HEIGHT, STATE_LENGTH))
+        convMod = keras.layers.Conv2D(64,4,activation='relu',data_format='channels_last')(convInput)
+        convMod = keras.layers.MaxPooling2D(pool_size=(2, 2))(convMod)
+        convMod = keras.layers.Conv2D(32, 2, activation='relu')(convMod)
+        convMod = keras.layers.MaxPooling2D(pool_size=(2, 2))(convMod)
+        convMod = keras.layers.Conv2D(32, 2, activation='relu')(convMod)
+        convMod = keras.layers.MaxPooling2D(pool_size=(2, 2))(convMod)
+        convMod = keras.layers.Flatten()(convMod)
+        convMod= keras.layers.Dense(512,activation='relu')(convMod)
+        convMod = keras.Model(inputs=convInput, outputs=convMod)
+
+        numInput = keras.layers.Input(shape=(NUMERIC_INPUT_LENGTH,))
+        numMod = keras.layers.Dense(14,activation='relu')(numInput)
+        numMod = keras.layers.Dense(8,activation='relu')(numMod)
+        numMod = keras.Model(inputs=numInput, outputs=numMod)
+
+        merge = keras.layers.concatenate([convMod.output, numMod.output])
+
+        mergeMod = keras.layers.Dense(256, activation='relu')(merge)
+        mergeMod = keras.layers.Dense(128, activation='relu')(mergeMod)
+        mergeMod = keras.layers.Dense(1, activation='sigmoid')(mergeMod)
+
+        self.model = keras.Model(inputs=[numMod.input,convMod.input], outputs=mergeMod)
         self.model.compile(loss="mean_squared_error", optimizer=Adam(lr=LEARNING_RATE))
-
 
     def save(self, name):
         self.model.save(name + '.h5')
@@ -52,7 +76,7 @@ def splitData(data): #TODO: Only does screen data right now. Rename to format wh
     #   concScreen = np.array([screenFactions, screenVision, screenSelected, screenHp, screenUnits, screenHeight])
     #   concRaw = np.array([frameID, minerals,vespene,supTotal,supUsed,supArmy,supWorkers])
     #   (concRaw, concMinimap, concScreen, winLoss)
-    return np.array([np.concatenate((data[1],data[2])),data[3]])
+    return (data[0], data[3], np.concatenate((data[1],data[2])))
 
 
 def createTrainingBatch(batchSize):
@@ -67,15 +91,16 @@ def createValidationBatch(batchSize):
         batch.append(splitData(reader.getRandomValidationState()))
     return fromSplitDataToVectors(batch)
 
+
 def fromSplitDataToVectors(data):
-    return (np.array([row[0] for row in data]),np.array([row[1] for row in data]))
+    return (np.array([row[0] for row in data]),np.array([row[1] for row in data]),np.array([row[2] for row in data]))
 
 for i in range(0,1000):
-    input, target = createTrainingBatch(450)
-    valInput, valTarget = createValidationBatch(100)
+    numInput, target,input = createTrainingBatch(4500)
+    valNumInput, valTarget, valInput = createValidationBatch(1000)
     #Swap ordering of dimensions so that keras can accept input.
     valInput= np.moveaxis(valInput, 1, 3)
     input = np.moveaxis(input, 1, 3)
 
-    network.model.fit(input, target, validation_data=(valInput,valTarget), epochs=1, batch_size=30)
+    network.model.fit([numInput, input], target, validation_data=([valNumInput, valInput],valTarget), epochs=1, batch_size=30)
     network.save(str(i))
