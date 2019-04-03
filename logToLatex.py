879a99176729820504f63f6963599507ca5f236f
ecen <ericgul@student.chalmers.se>
2019-04-03 09:58:40 +0200

    Adjusted logToLatex for DQN-1.

--- logToLatex.py
+++ logToLatex.py
@@ -1,12 +1,22 @@
 logFile = open("log.log", "r")
 
-line = logFile.readline()
+print("score time")
+#line = logFile.readline()
+#line = "line"
 
-while line:
+i = 0
+for line in logFile:
+    i += 1
+    if (i % 1 != 0): # Rendering all points takes time in LaTeX, skip some
+        continue
     words = line.split()
     score = words[0]
     time = words[2].replace("s", "")
-    print(score + " " + str(round(int(time)/60.0, 2)) + "")
-    line = logFile.readline()
+    minutes = int(time)/60.0
+    if (minutes > 280):
+         minutes = minutes - 146.5 # Adjust for pause in training
+    if (minutes > 1200):
+         minutes = minutes - 792.7 # Adjust for pause in training
+    print(score + " " + str(round(minutes, 2)) + "")
     
 logFile.close()
