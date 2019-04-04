6f0a1560f905b876fed9b3d49e32d4a22cfc0c9c
ecen <ericgul@student.chalmers.se>
2019-04-04 03:47:29 +0200

    Removed time modifiers.

--- logToLatex.py
+++ logToLatex.py
@@ -1,6 +1,6 @@
 logFile = open("log.log", "r")
 
-print("score time")
+#print("score time")
 #line = logFile.readline()
 #line = "line"
 
@@ -13,10 +13,6 @@ for line in logFile:
     score = words[0]
     time = words[2].replace("s", "")
     minutes = int(time)/60.0
-    if (minutes > 280):
-         minutes = minutes - 146.5 # Adjust for pause in training
-    if (minutes > 1200):
-         minutes = minutes - 792.7 # Adjust for pause in training
     print(score + " " + str(round(minutes, 2)) + "")
     
 logFile.close()
