931fec82a0cc5bef5b562ec0b5ef9579c6195e63
ecen <ericgul@student.chalmers.se>
2019-04-12 08:59:30 +0200

    Added second win-loss network.

--- matlab/supervisedWinLoss.m
+++ matlab/supervisedWinLoss.m
@@ -1,10 +1,23 @@
 data = load("data/win-loss.csv")
 [n, p] = size(data);
-m = 800
+m = n
 nr=data(1:m,1);
 trainLoss=data(1:m,2);
 validationLoss=data(1:m,3);
 
 scatter(nr, trainLoss, 20, validationLoss, 'filled')
+scatter(nr, validationLoss, 20, trainLoss, 'filled')
+colorbar
+
+%%
+
+data = load("data/win-loss-2.csv")
+[n, p] = size(data);
+m = n
+nr=data(1:m,1);
+trainLoss=data(1:m,2);
+validationLoss=data(1:m,3);
+
+%scatter(nr, trainLoss, 20, validationLoss, 'filled')
 scatter(nr, validationLoss, 20, trainLoss, 'filled')
 colorbar
