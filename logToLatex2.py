248815b9b4eb30aab2405321c9f5c6e3a777e80f
ecen <ericgul@student.chalmers.se>
2019-04-15 21:49:41 +0200

    First marine training.

--- logToLatex2.py
+++ logToLatex2.py
@@ -15,8 +15,12 @@ for line in logFile:
     time = words[2].replace("time=", "").replace("s", "")
     steps = words[3].replace("steps=", "").replace("\n", "")
     supplyWorkers = words[4].replace("supplyWorkers=", "").replace("\n", "")
+    supplyArmy = words[5].replace("supplyArmy=", "").replace("\n", "")
+    totalUnits = words[6].replace("totalUnits=", "").replace("\n", "")
+    totalUnitsKilled = words[7].replace("totalUnitsKilled=", "").replace("\n", "")
+    totalStructuresKilled = words[8].replace("totalStructuresKilled=", "").replace("\n", "")
     minutes = int(time)/60.0
     #print(score + " " + str(round(minutes, 2)) + "") # Time
-    print("{} {} {} {}".format(score, episodes, steps, supplyWorkers))
+    print("{} {} {} {} {} {} {} {}".format(score, episodes, steps, supplyWorkers, supplyArmy, totalUnits, totalUnitsKilled, totalStructuresKilled))
     
 logFile.close()
