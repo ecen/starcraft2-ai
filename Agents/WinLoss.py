4900a54afc271d350ff093d2091c0b298b348aa9
Aoldo <smurfpojken@gmail.com>
2019-04-30 08:25:44 +0200

    Remove obsolete code. Change network structure.

--- Agents/WinLoss.py
+++ Agents/WinLoss.py
@@ -30,61 +30,34 @@ class Network:
         convInput = keras.layers.Input(shape=(FRAME_WIDTH, FRAME_HEIGHT, STATE_LENGTH))
         convMod = keras.layers.Conv2D(9,(3,3),activation='relu',data_format='channels_last')(convInput)
         convMod = keras.layers.MaxPooling2D(pool_size=(2, 2))(convMod)
-        convMod = keras.layers.Conv2D(18, (3,3), activation='relu')(convMod)
+        convMod = keras.layers.Conv2D(9, (3,3), activation='relu')(convMod)
         convMod = keras.layers.MaxPooling2D(pool_size=(2, 2))(convMod)
-        convMod = keras.layers.Conv2D(36, (3,3), activation='relu')(convMod)
-        convMod = keras.layers.MaxPooling2D(pool_size=(2, 2))(convMod)
+        #convMod = keras.layers.Conv2D(36, (3,3), activation='relu')(convMod)
+        #convMod = keras.layers.MaxPooling2D(pool_size=(2, 2))(convMod)
         convMod = keras.layers.Flatten()(convMod)
-        convMod= keras.layers.Dense(128,activation='relu')(convMod)
-        convMod= keras.layers.Dense(56,activation='relu')(convMod)
+        convMod= keras.layers.Dense(32,activation='relu')(convMod)
+        convMod= keras.layers.Dense(16,activation='relu')(convMod)
         convMod = keras.Model(inputs=convInput, outputs=convMod)
 
         numInput = keras.layers.Input(shape=(NUMERIC_INPUT_LENGTH,))
-        numMod = keras.layers.Dense(14,activation='relu')(numInput)
-        numMod = keras.layers.Dense(8,activation='relu')(numMod)
+        numMod = keras.layers.Dense(5,activation='relu')(numInput)
+        numMod = keras.layers.Dense(5,activation='relu')(numMod)
         numMod = keras.Model(inputs=numInput, outputs=numMod)
 
         merge = keras.layers.concatenate([convMod.output, numMod.output])
 
-        mergeMod = keras.layers.Dense(128, activation='relu')(merge)
-        mergeMod = keras.layers.Dense(64, activation='relu')(mergeMod)
+        mergeMod = keras.layers.Dense(16, activation='relu')(merge)
+        mergeMod = keras.layers.Dense(16, activation='relu')(mergeMod)
         mergeMod = keras.layers.Dense(1, activation='sigmoid')(mergeMod)
 
         self.model = keras.Model(inputs=[numMod.input,convMod.input], outputs=mergeMod)
         self.model.compile(loss="mean_absolute_error", optimizer=Adam(lr=LEARNING_RATE))
-
+        self.model = keras.models.load_model("46-L[0.2532796633808965].h5")
     def save(self, name):
         self.model.save(name + '.h5')
 
 # init of network
 network = Network()
-
-
-
-
-#TODO: Change this completely so that each epoch iterates over the entire dataset somehow
-def createTrainingBatch(batchSize):
-    batch = []
-    for i in range (0,batchSize):
-        temp = reader.getRandomTrainingState()
-        if temp != None:
-            batch.append(temp)
-    return transposeBatch(batch)
-
-def createValidationBatch(batchSize):
-    batch = []
-    for i in range(0, batchSize):
-        temp = reader.getRandomValidationState()
-        if temp != None:
-            batch.append(temp)
-    return transposeBatch(batch)
-
-#Extracts each column from "splitData" function and makes them
-#into rows instead. Effectively converting from [replayID][type of data]
-#to [type of data][replayID]
-def transposeBatch(data):
-    return (np.array([row[0] for row in data]),np.array([row[1] for row in data]),np.array([row[2] for row in data]))
-
 
 if __name__ == "__main__":
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H.%M.%S")
